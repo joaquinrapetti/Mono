fe360bfd6c00a47b9f8e131a38307f4bce6fa64f
joaquinrapetti <j.rapetti92@gmail.com>
2021-03-05 20:40:24 -0300

    canvas01

--- dom/codigo.js
+++ dom/codigo.js
@@ -1,4 +1,8 @@
 alert("Prueba");
+var fondoImagen = document.getElementById("fondoMono");
+var papel = fondoImage.getContext("2d");
+var mapa = "tile.png";
+
 var ruta = window.location;
 console.log(ruta);
 document.write("Estás en: " + ruta);
