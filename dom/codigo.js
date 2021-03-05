96d74833db9c533a13d1d311101582fe86e8569a
NachoSupparo <suppa92@gmail.com>
2021-03-05 20:12:01 -0300

    Test 2

--- dom/codigo.js
+++ dom/codigo.js
@@ -1,4 +1,4 @@
-alert("Empiezan a aparecer los conocedores");
+alert("Empiezan a aparecer los conocedores_Martin Puto");
 var ruta = window.location;
 console.log(ruta);
 document.write("Estás en: " + ruta);
