5db36f20677c2e39c3990fad654f029cbfaf3933
NachoSupparo <suppa92@gmail.com>
2021-03-05 20:52:15 -0300

    Aleatorio

--- dom/codigo.js
+++ dom/codigo.js
@@ -1,7 +1,23 @@
 alert("Prueba");
 var fondoImagen = document.getElementById("fondoMono");
-var papel = fondoImage.getContext("2d");
+var papel = vp.getContext("2d");
 var mapa = "tile.png";
+
+var img = new Image()
+imagen.src = mapa;
+imagen.addEventListener("load", dibujar) 
+
+function dibujar()
+{
+    papel.drawImage(imagen, 100, 1-0)
+}
+
+function aleatorio (min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min
+    return resultado;
+}
 
 var ruta = window.location;
 console.log(ruta);
