6e35f1c1678dff0c9826c69cdd8c76dd4ee3872f
joaquinrapetti <j.rapetti92@gmail.com>
2021-03-05 21:07:25 -0300

    com

--- dom/codigo.js
+++ dom/codigo.js
@@ -3,19 +3,19 @@ var fondoImagen = document.getElementById("fondoMono");
 var papel = vp.getContext("2d");
 var mapa = "tile.png";
 
-var img = new Image()
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar) 
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujar); 
 
 function dibujar()
 {
-    papel.drawImage(imagen, 100, 1-0)
+    papel.drawImage(fondo, 0, 0);
 }
 
-function aleatorio (min, maxi)
+function aleatorio(min, maxi)
 {
     var resultado;
-    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
     return resultado;
 }
 
